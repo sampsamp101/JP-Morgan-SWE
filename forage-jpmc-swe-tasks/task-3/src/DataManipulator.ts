c9a4baf23005b326f09f1d587c0bd578b19a0fb0
Aaron Long Jiwei <aaronjiwei@gmail.com>
2024-12-26 00:48:51 +0800

    Merged task 1, 2, and 3 into a single repository

---
+++ forage-jpmc-swe-tasks/task-3/src/DataManipulator.ts
@@ -0,0 +1,33 @@
+import { ServerRespond } from './DataStreamer';
+
+export interface Row {
+  price_abc: number,
+  price_def: number,
+  ratio: number,
+  timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
+}
+
+export class DataManipulator {
+  static generateRow(serverRespond: ServerRespond[]) : Row {
+
+  const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price)/2;
+  const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price)/2;
+  const ratio = priceABC / priceDEF;
+  const upperbound = 1 + (5/100)*1;
+  const lowerbound = 1 - (5/100)*1;
+ 
+  return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ? 
+      serverRespond[0].timestamp: serverRespond[1].timestamp,
+      upper_bound: upperbound,
+      lower_bound: lowerbound,
+      trigger_alert: (ratio > upperbound || ratio < lowerbound) ? ratio : undefined,
+    };
+  }
+}
